0b4b3e367d4d97b0fa98007629f3843b067f6ada
Max Irwin <max@Admins-MacBook-Pro-3.local>
2019-01-06 10:48:10 -0500

    Basic search notebook and cleanup

---
+++ locations/tmdbMovies.py
@@ -0,0 +1,39 @@
+import json
+
+
+def rawTmdbMovies(filename):
+    return json.load(open(filename))
+
+
+def writeTmdmMovies(rawMoviesJson, path):
+    with open(path, 'w') as f:
+        json.dump(rawMoviesJson, f)
+
+def tmdbMovies(filename='tmdb.json'):
+    tmdbMovies = rawTmdbMovies(filename)
+    for movieId, tmdbMovie in tmdbMovies.items():
+        yield (movieId, tmdbMovie)
+
+
+def indexableMovies(filename='tmdb.json'):
+    """ Generates TMDB movies, similar to how ES Bulk indexing
+        uses a generator to generate bulk index/update actions """
+    for movieId, tmdbMovie in tmdbMovies(filename):
+        try:
+            releaseDate = None
+            if 'release_date' in tmdbMovie and len(tmdbMovie['release_date']) > 0:
+                releaseDate = tmdbMovie['release_date'] + 'T00:00:00Z'
+
+            yield {'id': movieId,
+                   'title': tmdbMovie['title'],
+                   'overview': tmdbMovie['overview'],
+                   'tagline': tmdbMovie['tagline'],
+                   'directors': [director['name'] for director in tmdbMovie['directors']],
+                   'cast': [castMember['name'] for castMember in tmdbMovie['cast']],
+                   'genres': [genre['name'] for genre in tmdbMovie['genres']],
+                   'release_date': releaseDate,
+                   'vote_average': tmdbMovie['vote_average'] if 'vote_average' in tmdbMovie else None,
+                   'vote_count': int(tmdbMovie['vote_count']) if 'vote_count' in tmdbMovie else None,
+                   }
+        except KeyError as k: # Ignore any movies missing these attributes
+            continue
